18abd8fa316d1e08f9f749b1e4f433d80670efc5
Cookie Cakes <108630416+FatherCheese@users.noreply.github.com>
2025-06-06 12:48:45 -0700

    Commit One - Remove existing plankton system
    * Removed all plankton entities, components, and systems
    * Moved plankton scanner texture

--- Content.Shared/Chemistry/Reagent/ReagentPrototype.cs
+++ Content.Shared/Chemistry/Reagent/ReagentPrototype.cs
@@ -18,7 +18,6 @@ using Robust.Shared.Random;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
 using Robust.Shared.Utility;
-using Content.Shared.Plankton;
 
 namespace Content.Shared.Chemistry.Reagent
 {
@@ -101,26 +100,6 @@ namespace Content.Shared.Chemistry.Reagent
         [DataField]
         public bool MetamorphicChangeColor { get; private set; } = true;
 
-
-    // List of plankton components this reagent can contain
-    public List<PlanktonComponent> PlanktonComponents { get; set; } = new List<PlanktonComponent>();
-
-    // Method to check if the reagent contains plankton
-    public bool HasPlankton => PlanktonComponents.Any();
-
-    // Method to add plankton to the reagent (to be added in a solution later)
-    public void AddPlankton(PlanktonComponent plankton)
-    {
-        PlanktonComponents.Add(plankton);
-    }
-
-    // Method to remove plankton from the reagent
-    public void RemovePlankton(PlanktonComponent plankton)
-    {
-        PlanktonComponents.Remove(plankton);
-    }
-
-
         /// <summary>
         /// If not null, makes something slippery. Also defines slippery interactions like stun time and launch mult.
         /// </summary>
@@ -166,16 +145,16 @@ namespace Content.Shared.Chemistry.Reagent
         [DataField]
         public bool WorksOnTheDead;
 
-        [DataField(serverOnly: true)]
+        [DataField]
         public FrozenDictionary<ProtoId<MetabolismGroupPrototype>, ReagentEffectsEntry>? Metabolisms;
 
-        [DataField(serverOnly: true)]
+        [DataField]
         public Dictionary<ProtoId<ReactiveGroupPrototype>, ReactiveReagentEffectEntry>? ReactiveEffects;
 
         [DataField(serverOnly: true)]
         public List<ITileReaction> TileReactions = new(0);
 
-        [DataField("plantMetabolism", serverOnly: true)]
+        [DataField("plantMetabolism")]
         public List<EntityEffect> PlantMetabolisms = new(0);
 
         [DataField]
